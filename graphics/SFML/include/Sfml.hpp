36c45803067c226692d1c4e3bf566c0dcdd08a7f
baptiste <baptiste.dunes@epitech.eu>
2025-04-10 22:55:41 +0200

    feat: sfml displaying using % and major events managed

--- graphics/SFML/include/Sfml.hpp
+++ graphics/SFML/include/Sfml.hpp
@@ -8,11 +8,14 @@
 #ifndef SFML_HPP_
     #define SFML_HPP_
 
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <iostream>
-#include "IGraphic.hpp"
-#include "updates.hpp"
+	#include <SFML/Graphics.hpp>
+	#include <SFML/Window.hpp>
+	#include <iostream>
+	#include "IGraphic.hpp"
+	#include "updates.hpp"
+
+	#define WIN_SIZE_X 1700
+	#define WIN_SIZE_Y 1700
 
 namespace arcade {
 	class Sfml : public IGraphic {
@@ -21,8 +24,11 @@ namespace arcade {
 			~Sfml() override;
 			event_t getEvent() override;
 			void display(data_t data) override;
+			void drawSprites(std::vector<entity_t> entities);
+			void drawTexts(std::vector<text_t> texts);
 
 		private:
+			static const std::map<int, event_e> _conversionMap;
 			sf::RenderWindow _window;
 			sf::Font _font;
 	};
--- graphics/SFML/src/Sfml.cpp
+++ graphics/SFML/src/Sfml.cpp
@@ -7,11 +7,42 @@
 
 #include "Sfml.hpp"
 
-arcade::Sfml::Sfml() {
-    _window.create(sf::VideoMode(1000, 1000), "Arcade SFML");
-    _window.setFramerateLimit(60);
+const std::map<int, event_e> arcade::Sfml::_conversionMap = {
+    {sf::Keyboard::Escape, A_KEY_ESC},
+    {sf::Keyboard::Enter, A_KEY_ENTER},
+    {sf::Keyboard::Space, A_KEY_SPACE},
+    {sf::Keyboard::A, A_KEY_A},
+    {sf::Keyboard::B, A_KEY_B},
+    {sf::Keyboard::C, A_KEY_C},
+    {sf::Keyboard::D, A_KEY_D},
+    {sf::Keyboard::E, A_KEY_E},
+    {sf::Keyboard::F, A_KEY_F},
+    {sf::Keyboard::G, A_KEY_G},
+    {sf::Keyboard::H, A_KEY_H},
+    {sf::Keyboard::I, A_KEY_I},
+    {sf::Keyboard::J, A_KEY_J},
+    {sf::Keyboard::K, A_KEY_K},
+    {sf::Keyboard::L, A_KEY_L},
+    {sf::Keyboard::M, A_KEY_M},
+    {sf::Keyboard::N, A_KEY_N},
+    {sf::Keyboard::O, A_KEY_O},
+    {sf::Keyboard::P, A_KEY_P},
+    {sf::Keyboard::Q, A_KEY_Q},
+    {sf::Keyboard::R, A_KEY_R},
+    {sf::Keyboard::S, A_KEY_S},
+    {sf::Keyboard::T, A_KEY_T},
+    {sf::Keyboard::U, A_KEY_U},
+    {sf::Keyboard::V, A_KEY_V},
+    {sf::Keyboard::W, A_KEY_W},
+    {sf::Keyboard::X, A_KEY_X},
+    {sf::Keyboard::Y, A_KEY_Y},
+    {sf::Keyboard::Z, A_KEY_Z}
+};
 
-    if (!_font.loadFromFile("./lib/assets/arcade_sfml/arial.ttf")) {
+arcade::Sfml::Sfml() {
+    _window.create(sf::VideoMode(WIN_SIZE_X, WIN_SIZE_Y), "Arcade SFML");
+
+    if (!_font.loadFromFile("./lib/assets/arcade_menu/font/arcade.ttf")) {
         std::cerr << "Error: Can't load font" << std::endl;
     }
 }
@@ -23,86 +54,73 @@ arcade::Sfml::~Sfml() {
 event_t arcade::Sfml::getEvent() {
     event_t events;
     sf::Event sfEvent;
+
     while (_window.pollEvent(sfEvent)) {
         if (sfEvent.type == sf::Event::Closed) {
             events.events.push_back(A_KEY_C);
         }
         else if (sfEvent.type == sf::Event::KeyPressed) {
-            switch (sfEvent.key.code) {
-                case sf::Keyboard::Z:
-                    events.events.push_back(A_KEY_Z);
-                    break;
-                case sf::Keyboard::S:
-                    events.events.push_back(A_KEY_S);
-                    break;
-                case sf::Keyboard::Q:
-                    events.events.push_back(A_KEY_Q);
-                    break;
-                case sf::Keyboard::D:
-                    events.events.push_back(A_KEY_D);
-                    break;
-                default:
-                    break;
+            for (std::map<int, event_e>::const_iterator it = _conversionMap.begin(); it != _conversionMap.end(); ++it) {
+                if (it->first == sfEvent.key.code) {
+                    events.events.push_back(it->second);
+                }
             }
         }
     }
     return events;
 }
 
-void arcade::Sfml::display(data_t data) {
-    _window.clear(sf::Color::Black);
-
-    for (entity_t bg : data.bg) {
-        sf::Sprite sprite;
-        sf::Texture texture;
-        texture.loadFromFile(bg.asset);
-        sprite.setTexture(texture);
-        sprite.setPosition(1000 / bg.pos.x, 1000 / bg.pos.y);
-        sf::FloatRect spriteBounds = sprite.getLocalBounds();
-        sf::Vector2f scale;
-        scale.x = (1000 / 100 * bg.size.x) / spriteBounds.width;
-        scale.y = (1000 / 100 * bg.size.y) / spriteBounds.height;
-        sprite.setScale(scale);
-        _window.draw(sprite);
+void arcade::Sfml::drawSprites(std::vector<entity_t> entities) {
+    for (entity_t entity : entities) {
+        if (entity.asset == "") {
+            sf::RectangleShape rectangle({static_cast<float>(WIN_SIZE_X / 100 * entity.size.x), static_cast<float>(WIN_SIZE_Y / 100 * entity.size.y)});
+            rectangle.setPosition(WIN_SIZE_X / 100 * entity.pos.x, WIN_SIZE_Y / 100 * entity.pos.y);
+            sf::Color color;
+            color.r = entity.color.r;
+            color.g = entity.color.g;
+            color.b = entity.color.b;
+            color.a = entity.color.a;
+            rectangle.setFillColor(color);
+            _window.draw(rectangle);
+        } else {
+            sf::Sprite sprite;
+            sf::Texture texture;
+            texture.loadFromFile(entity.asset);
+            sprite.setTexture(texture);
+            sprite.setPosition(WIN_SIZE_X / 100 * entity.pos.x, WIN_SIZE_Y / 100 * entity.pos.y);
+            sf::FloatRect spriteBounds = sprite.getLocalBounds();
+            sf::Vector2f scale;
+            scale.x = (WIN_SIZE_X / 100 * entity.size.x) / spriteBounds.width;
+            scale.y = (WIN_SIZE_Y / 100 * entity.size.y) / spriteBounds.height;
+            sprite.setScale(scale);
+            _window.draw(sprite);
+        }
     }
+}
 
-    for (entity_t objects : data.objects) {
-        sf::Sprite sprite;
-        sf::Texture texture;
-        texture.loadFromFile(objects.asset);
-        sprite.setTexture(texture);
-        sprite.setPosition(1000 / objects.pos.x, 1000 / objects.pos.y);
-        sf::FloatRect spriteBounds = sprite.getLocalBounds();
-        sf::Vector2f scale;
-        scale.x = (1000 / 100 * objects.size.x) / spriteBounds.width;
-        scale.y = (1000 / 100 * objects.size.y) / spriteBounds.height;
-        sprite.setScale(scale);
-        _window.draw(sprite);
-    }
-
-    for (entity_t ui : data.ui) {
-        sf::Sprite sprite;
-        sf::Texture texture;
-        texture.loadFromFile(ui.asset);
-        sprite.setTexture(texture);
-        sprite.setPosition(1000 / ui.pos.x, 1000 / ui.pos.y);
-        sf::FloatRect spriteBounds = sprite.getLocalBounds();
-        sf::Vector2f scale;
-        scale.x = (1000 / 100 * ui.size.x) / spriteBounds.width;
-        scale.y = (1000 / 100 * ui.size.y) / spriteBounds.height;
-        sprite.setScale(scale);
-        _window.draw(sprite);
-    }
-
-    for (text_t txt : data.texts) {
+void arcade::Sfml::drawTexts(std::vector<text_t> texts) {
+    for (text_t txt : texts) {
         sf::Text text;
         text.setFont(_font);
         text.setString(txt.value);
         text.setCharacterSize(txt.fontSize);
-        text.setFillColor((const sf::Color&)txt.color);
-        text.setPosition(1000 / 100 * txt.pos.x, 1000 / 100 * txt.pos.y);
+        sf::Color color;
+        color.r = txt.color.r;
+        color.g = txt.color.g;
+        color.b = txt.color.b;
+        color.a = txt.color.a;
+        text.setFillColor(color);
+        text.setPosition(WIN_SIZE_X / 100 * txt.pos.x, WIN_SIZE_Y / 100 * txt.pos.y);
         _window.draw(text);
     }
-    
+}
+
+void arcade::Sfml::display(data_t data) {
+    _window.clear(sf::Color::Black);
+
+    drawSprites(data.bg);
+    drawSprites(data.objects);
+    drawSprites(data.ui);
+    drawTexts(data.texts);
     _window.display();
 }
