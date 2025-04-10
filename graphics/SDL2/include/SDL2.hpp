8678b45fcf47acbacf4550c139a615d1ef59d71a
JulesDELTEIL <jules.delteil@epitech.eu>
2025-04-10 16:49:29 +0200

    feat: sdl new input management

--- graphics/SDL2/include/SDL2.hpp
+++ graphics/SDL2/include/SDL2.hpp
@@ -20,64 +20,69 @@
     #define SIZE_ARRAY(a) (sizeof(a)/sizeof(a[0])) // Get size of no-empty any const array
     #define GET_VALUE(m, i) (m.find(i)->second) // Get value from map with index
 
-    #define RECT(e) ((SDL_Rect){(int)e.pos.x, (int)e.pos.y, (int)e.size.y, (int)e.size.x}) // Get the rectangle from an entity data
-    #define COLOR(e) ((SDL_Color){(Uint8)e.color.r, (Uint8)e.color.g, (Uint8)e.color.b}) // Get color from an entity or text
-    #define DRAW_RECT(re, e) (SDL_SetRenderDrawColor(re, e.color.r, e.color.g, e.color.b, e.color.a)) // Draw colored rectangle
-    #define RECT_TEXT(t) ((SDL_Rect){(int)t.pos.x, (int)t.pos.y, 0, 0}) // Get the rectangle from an text data
+    // Set SDL_Rect from entity data
+    #define RECT(e) ((SDL_Rect){(int)e.pos.x, (int)e.pos.y, (int)e.size.y, (int)e.size.x})
+    // Set SDL_Color from entity data
+    #define COLOR(e) ((SDL_Color){(Uint8)e.color.r, (Uint8)e.color.g, (Uint8)e.color.b})
+    // Draw colored rectangle
+    #define DRAW_RECT(re, e) (SDL_SetRenderDrawColor(re, e.color.r, e.color.g, e.color.b, e.color.a))
+    // Set SDL_Rect from text data
+    #define RECT_TEXT(t) ((SDL_Rect){WINDOW_SIZE_X * (int)t.pos.x / 100, WINDOW_SIZE_Y * (int)t.pos.y / 100, \
+        ((int)SIZE_ARRAY(t.value) * (int)t.fontSize) / 2, t.fontSize})
 
     #define NOT_SET -1
 
 const std::map<int, event_e> KEYS = {
-    {SDL_SCANCODE_ESCAPE, A_KEY_ESC},
-    {SDL_SCANCODE_SPACE, A_KEY_SPACE},
-    {SDL_SCANCODE_RETURN, A_KEY_DEL},
-    {SDL_SCANCODE_BACKSPACE, A_KEY_ENTER},
-    {SDL_SCANCODE_TAB, A_KEY_TAB},
-    {SDL_SCANCODE_LCTRL, A_KEY_CTRL},
-    {SDL_SCANCODE_LSHIFT, A_KEY_SHIFT},
+    {SDLK_ESCAPE, A_KEY_ESC},
+    {SDLK_SPACE, A_KEY_SPACE},
+    {SDLK_BACKSPACE, A_KEY_DEL},
+    {SDLK_RETURN, A_KEY_ENTER},
+    {SDLK_TAB, A_KEY_TAB},
+    {SDLK_LCTRL, A_KEY_CTRL},
+    {SDLK_LSHIFT, A_KEY_SHIFT},
     {NOT_SET, A_KEY_SHIFTLOCK},
-    {SDL_SCANCODE_LALT, A_KEY_ALT},
+    {SDLK_LALT, A_KEY_ALT},
     {NOT_SET, A_KEY_SQUARE},
-    {SDL_SCANCODE_UP, A_KEY_UP},
-    {SDL_SCANCODE_DOWN, A_KEY_DOWN},
-    {SDL_SCANCODE_LEFT, A_KEY_LEFT},
-    {SDL_SCANCODE_RIGHT, A_KEY_RIGHT},
-    {SDL_SCANCODE_A, A_KEY_A},
-    {SDL_SCANCODE_B, A_KEY_B},
-    {SDL_SCANCODE_C, A_KEY_C},
-    {SDL_SCANCODE_D, A_KEY_D},
-    {SDL_SCANCODE_E, A_KEY_E},
-    {SDL_SCANCODE_F, A_KEY_F},
-    {SDL_SCANCODE_G, A_KEY_G},
-    {SDL_SCANCODE_H, A_KEY_H},
-    {SDL_SCANCODE_I, A_KEY_I},
-    {SDL_SCANCODE_J, A_KEY_J},
-    {SDL_SCANCODE_K, A_KEY_K},
-    {SDL_SCANCODE_L, A_KEY_L},
-    {SDL_SCANCODE_M, A_KEY_M},
-    {SDL_SCANCODE_N, A_KEY_N},
-    {SDL_SCANCODE_O, A_KEY_O},
-    {SDL_SCANCODE_P, A_KEY_P},
-    {SDL_SCANCODE_Q, A_KEY_Q},
-    {SDL_SCANCODE_R, A_KEY_R},
-    {SDL_SCANCODE_S, A_KEY_S},
-    {SDL_SCANCODE_T, A_KEY_T},
-    {SDL_SCANCODE_U, A_KEY_U},
-    {SDL_SCANCODE_V, A_KEY_V},
-    {SDL_SCANCODE_W, A_KEY_W},
-    {SDL_SCANCODE_X, A_KEY_X},
-    {SDL_SCANCODE_Y, A_KEY_Y},
-    {SDL_SCANCODE_Z, A_KEY_Z},
-    {SDL_SCANCODE_0, A_KEY_0},
-    {SDL_SCANCODE_1, A_KEY_1},
-    {SDL_SCANCODE_2, A_KEY_2},
-    {SDL_SCANCODE_3, A_KEY_3},
-    {SDL_SCANCODE_4, A_KEY_4},
-    {SDL_SCANCODE_5, A_KEY_5},
-    {SDL_SCANCODE_6, A_KEY_6},
-    {SDL_SCANCODE_7, A_KEY_7},
-    {SDL_SCANCODE_8, A_KEY_8},
-    {SDL_SCANCODE_9, A_KEY_9},
+    {SDLK_UP, A_KEY_UP},
+    {SDLK_DOWN, A_KEY_DOWN},
+    {SDLK_LEFT, A_KEY_LEFT},
+    {SDLK_RIGHT, A_KEY_RIGHT},
+    {SDLK_a, A_KEY_A},
+    {SDLK_b, A_KEY_B},
+    {SDLK_c, A_KEY_C},
+    {SDLK_d, A_KEY_D},
+    {SDLK_e, A_KEY_E},
+    {SDLK_f, A_KEY_F},
+    {SDLK_g, A_KEY_G},
+    {SDLK_h, A_KEY_H},
+    {SDLK_i, A_KEY_I},
+    {SDLK_j, A_KEY_J},
+    {SDLK_k, A_KEY_K},
+    {SDLK_l, A_KEY_L},
+    {SDLK_m, A_KEY_M},
+    {SDLK_n, A_KEY_N},
+    {SDLK_o, A_KEY_O},
+    {SDLK_p, A_KEY_P},
+    {SDLK_q, A_KEY_Q},
+    {SDLK_r, A_KEY_R},
+    {SDLK_s, A_KEY_S},
+    {SDLK_t, A_KEY_T},
+    {SDLK_u, A_KEY_U},
+    {SDLK_v, A_KEY_V},
+    {SDLK_w, A_KEY_W},
+    {SDLK_x, A_KEY_X},
+    {SDLK_y, A_KEY_Y},
+    {SDLK_z, A_KEY_Z},
+    {SDLK_0, A_KEY_0},
+    {SDLK_1, A_KEY_1},
+    {SDLK_2, A_KEY_2},
+    {SDLK_3, A_KEY_3},
+    {SDLK_4, A_KEY_4},
+    {SDLK_5, A_KEY_5},
+    {SDLK_6, A_KEY_6},
+    {SDLK_7, A_KEY_7},
+    {SDLK_8, A_KEY_8},
+    {SDLK_9, A_KEY_9},
     {NOT_SET, A_MOUSE_LEFT},
     {NOT_SET, A_MOUSE_RIGHT},
     {NOT_SET, A_MOUSE_MIDDLE},
@@ -98,6 +103,7 @@ class SDL2 : public IGraphic{
         void display(data_t) override;
 
     private:
+        bool keyNotAllreadyPressed(event_e);
         void eventManager(void);
         void displayText(text_t text_data);
         void displayEntity(entity_t entity);
--- graphics/SDL2/src/SDL2.cpp
+++ graphics/SDL2/src/SDL2.cpp
@@ -15,25 +15,44 @@ arcade::SDL2::SDL2()
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
         throw "Error: init SDL";
     _window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED, WINDOW_SIZE_X, WINDOW_SIZE_Y, SDL_WINDOW_FULLSCREEN);
+        SDL_WINDOWPOS_UNDEFINED, WINDOW_SIZE_X, WINDOW_SIZE_Y, SDL_WINDOW_SHOWN);
     if (_window == NULL)
         throw "Error: init SDL window";
     _renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED);
     if (_renderer == NULL)
         throw "Error: init SDL renderer failed";
+    TTF_Init();
+    IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
     SDL_SetRenderDrawColor(_renderer, 0, 0, 0, 0);
 }
 
 arcade::SDL2::~SDL2()
 {
+    TTF_Quit();
+    IMG_Quit();
     SDL_DestroyRenderer(_renderer);
     SDL_DestroyWindow(_window);
     SDL_Quit();
 }
 
+#include <iostream>
 event_t arcade::SDL2::getEvent(void)
 {
-    return _events;
+    event_t giveEvent = _events;
+
+    for (std::size_t i = 0; i < _events.events.size(); ++i) {
+        std::cout << _events.events[i] << std::endl;
+    }
+    _events.events.clear();
+    return giveEvent;
+}
+
+bool arcade::SDL2::keyNotAllreadyPressed(event_e event)
+{
+    for (std::size_t i = 0; i < _events.events.size(); ++i)
+        if (event == _events.events[i])
+            return false;
+    return true;
 }
 
 void arcade::SDL2::eventManager(void)
@@ -43,11 +62,15 @@ void arcade::SDL2::eventManager(void)
     if (SDL_PollEvent(&_SDLevent)) {
         if (_SDLevent.type == SDL_QUIT)
             exit(0);
+        if (_SDLevent.type == SDL_KEYDOWN)
+            if (keyNotAllreadyPressed(GET_VALUE(KEYS, _SDLevent.key.keysym.sym))) {
+                _events.events.push_back(GET_VALUE(KEYS, _SDLevent.key.keysym.sym));
+            }
     }
     keystate = SDL_GetKeyboardState(NULL);
     if (keystate != NULL) {
         for (std::size_t i = 0; i < SIZE_ARRAY(keystate); ++i) {
-            if (keystate[i]) {
+            if (keystate[i] && keyNotAllreadyPressed(GET_VALUE(KEYS, i))) {
                 _events.events.push_back(GET_VALUE(KEYS, i));
             }
         }
@@ -73,19 +96,10 @@ void arcade::SDL2::displayEntity(entity_t entity_data)
     }
 }
 
-#include <iostream>
 void arcade::SDL2::displayText(text_t text_data)
 {
-    std::cout << \
-        text_data.fontPath.c_str() << " - " << (int)text_data.fontSize \
-        << " / " << text_data.pos.x << " " << text_data.pos.y \
-        << std::endl;
-    if (std::filesystem::exists(text_data.fontPath))
-        std::cout << "ahah\n";
     TTF_Font* font = TTF_OpenFont(text_data.fontPath.c_str(),
         (int)text_data.fontSize);
-    if (font == NULL)
-        std::cout << "KO" << std::endl;
     SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font,
         text_data.value.c_str(), COLOR(text_data));
     SDL_Texture* msg = SDL_CreateTextureFromSurface(_renderer, surfaceMessage);
